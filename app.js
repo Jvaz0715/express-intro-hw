52f5245feea71abd28c48e8880e6cd6065b70d69
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-05-18 07:48:21 -0400

    Completes through step #13 of assignment

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
-//above brings in express
+//#6 above brings in express
 
 const logger = require("morgan");
-//above brings in morgan
+//#6 above brings in morgan
 
 const path = require("path");
-//above brings in ejs
+// #6 above brings in ejs
 
 const app = express();
-// the above asigns express() function to variable app for cleaner code
+// the above assigns express() function to variable app for cleaner code
 // app will now be used to get, post, etc
+
+// #7 create server using express and use logger
+app.use(logger("dev"));
+app.use(express.json());
+
+// #9 after creating 'views' folder, make sure the views folder and view engine are connected to ejs through path
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// #10 after creating a 'public' folder, connect it here
+app.use(express.static(path.join(__dirname, "public")))
