cee92dd76560502f5e24b0aa8cb6fb3f25356ff4
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-05-18 07:36:34 -0400

    app.js brings in express, morgan and ejs and assigns them to variables for cleaner code

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const express = require("express");
 
 const logger = require("morgan");
 //above brings in morgan
+
+const path = require("path");
+//above brings in ejs
+
+const app = express();
+// the above asigns express() function to variable app for cleaner code
+// app will now be used to get, post, etc
